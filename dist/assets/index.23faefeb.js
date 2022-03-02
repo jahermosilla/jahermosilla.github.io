2bfa0be867edd72926b1cb79044d5f8aecfd7212
jahermosilla <josehermosillarodrigo@gmail.com>
2022-03-02 14:04:52 +0100

    deploy dist to gh-pages branch

---
+++ dist/assets/index.23faefeb.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./app.ff11837d.js";import{c as r,o}from"./vendor.9ea455b5.js";const c={};function n(a,t){return o(),r("h4",null,"Blog")}var _=e(c,[["render",n]]);export{_ as default};
